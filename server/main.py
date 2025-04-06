2b779f081dd9d88fb3167d2cfeaf70aa5589d9d9
Ayush Bohra <codefingers809@gmail.com>
2025-04-06 07:57:15 +0530

    tts

--- server/main.py
+++ server/main.py
@@ -6,7 +6,29 @@ from flask import Flask
 from flask_cors import CORS
 
 # Now import from app.routes
-from routes import json_formatter, image_processing, password_tools ,network_tools , csv_excel, random_tools , code_formatting, markdown_formatting, api_tester, markdown_formatting,link_shortner , qrcode, currency_converter, time_converter, document_tools, domain_lookup, ip_lookup, email_lookup
+from routes import (
+    json_formatter,
+    image_processing,
+    password_tools,
+    network_tools,
+    csv_excel,
+    random_tools,
+    code_formatting,
+    markdown_formatting,
+    api_tester,
+    markdown_formatting,
+    link_shortner,
+    qrcode,
+    currency_converter,
+    time_converter,
+    document_tools,
+    domain_lookup,
+    ip_lookup,
+    email_lookup,
+    audio_converter,
+    tts_service,
+)
+
 app = Flask(__name__)
 CORS(app)
 
@@ -28,8 +50,11 @@ document_tools.register(app)
 ip_lookup.register(app)
 domain_lookup.register(app)
 email_lookup.register(app)
+audio_converter.register(app)
+tts_service.register(app)
 
-@app.route('/')
+
+@app.route("/")
 def home():
     return {"message": "DevTools API running 🎯"}
 
